9ba351bbe4e8647ae640c970e0fd6137a626b0e3
Thiago Almeida <85900124+thiagoalmeidas@users.noreply.github.com>
2022-04-11 19:26:48 -0300

    update

--- src/components/stack/index.jsx
+++ src/components/stack/index.jsx
@@ -1,6 +1,6 @@
 function Stack(props) {
     return (
-        <div className="border  border-info rounded p-2">{props.stackgraphic}</div>
+        <div className="border border-info text-white rounded p-2">{props.stackgraphic}</div>
     );
 }
 export default Stack;
--- src/pages/graphics/index.jsx
+++ src/pages/graphics/index.jsx
@@ -9,10 +9,10 @@ function Graphics() {
             <section className="wow animate__fadeIn">
                 <div className="flex-fill p-5">
                     <div className="card-deck d-flex flex-wrap gap-4 justify-content-center">
-                        <Card stackgraphic='Figma - Illustrator - Photoshop' title='Murb' desc='Criação de logotipo em vetor e identidade visual para marca de roupas StreetWear.' image='img/logo-murb.jpg'/>
-                        <Card stackgraphic='Figma - Illustrator - Photoshop' title='Vanessa Hair' desc='Criação de logotipo em vetor e identidade visual para salão de caleireiros.' image='img/logo-vanessa.jpg'/>
-                        <Card stackgraphic='Figma - Illustrator - Photoshop' title='Comunidade Raizes' desc='Criação de logotipo em vetor e identidade visual para Igreja Comunidade Raizes.' image='img/logo-comunidade-raizes.jpg'/>
-                        <Card stackgraphic='Figma - Illustrator - Photoshop' title='Andor Refugiados' desc='Criação de logotipo em vetor e identidade visual para o projeto Andor Refugiados.' image='img/logo-andor.jpg'/>
+                        <Card stack1='Figma' stack2='Illustrator' stack3='Photoshop' title='Murb' desc='Criação de logotipo em vetor e identidade visual para marca de roupas StreetWear.' image='img/logo-murb.jpg'/>
+                        <Card stack1='Figma' stack2='Illustrator' stack3='Photoshop' title='Vanessa Hair' desc='Criação de logotipo em vetor e identidade visual para salão de caleireiros.' image='img/logo-vanessa.jpg'/>
+                        <Card stack1='Figma' stack2='Illustrator' stack3='Photoshop' title='Comunidade Raizes' desc='Criação de logotipo em vetor e identidade visual para Igreja Comunidade Raizes.' image='img/logo-comunidade-raizes.jpg'/>
+                        <Card stack1='Figma' stack2='Illustrator' stack3='Photoshop' title='Andor Refugiados' desc='Criação de logotipo em vetor e identidade visual para o projeto Andor Refugiados.' image='img/logo-andor.jpg'/>
                     </div>
                 </div>
             </section>
